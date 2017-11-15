1899931cec1550aaed69d1f0117b50c7077e1972
richmartins <rvmartin@it3pc1.epfl.ch>
2017-11-15 16:27:29 +0100

    make the btnScan work

--- src/whoIsThereP/main.java
+++ src/whoIsThereP/main.java
@@ -7,34 +7,7 @@ import java.net.UnknownHostException;
 
 public class main {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		//Window w1 = new Window();
-		String localhost = Inet4Address.getLocalHost().getHostAddress();
-		String goodlocalhost = localhost.length() < 9 ? localhost: localhost.substring(0, 9);
 		
-
-		//localhost.substring(0, 9);
-		//System.out.println(localhost);
-		checkHosts("172.22.10");
-		
-		int toto = 120;
-		String tata = "titi";
-		
-	}
-	
-	public static void checkHosts(String subnet) throws UnknownHostException, IOException{
-		int timeout = 20;
-		for(int i = 1; i<256; i++){
-			String host = subnet+"."+i;
-			if(InetAddress.getByName(host).isReachable(timeout)){
-				InetAddress hostname = InetAddress.getByName(host);
-				String name = hostname.getHostName();
-				System.out.println("IP : " + host + " Host_Name : " + name + " is Reachable");
-				
-				
-			}else{
-				//System.out.println("is not in the if : " + host);
-			}
-		}
-		System.out.println("Scan finished");
+		Window w1 = new Window();
 	}
 }
