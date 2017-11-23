f4b25efc2e1afcfa394ca96b7f234dd8bcf90926
rvmartin <rvmartin@it3pc1.epfl.ch>
2017-11-23 08:29:45 +0100

    javaFX implemented

--- src/whoIsThereP/main.java
+++ src/whoIsThereP/main.java
@@ -7,7 +7,25 @@ import java.net.UnknownHostException;
 
 public class main {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		
-		Window w1 = new Window();
+		String localhost = Inet4Address.getLocalHost().getHostAddress();
+		String goodlocalhost = localhost.length() < 9 ? localhost: localhost.substring(0, 10);
+
+		checkHosts(goodlocalhost);
+	}
+	
+	public static void checkHosts(String subnet) throws UnknownHostException, IOException{
+		int timeout = 1000;
+		for(int i = 1; i < 255; i++){
+			String host = subnet+"."+i;
+			if(InetAddress.getByName(host).isReachable(timeout)){
+				InetAddress hostname = InetAddress.getByName(host);
+				String name = hostname.getHostName();
+				System.out.println("IP : " + host + " Host_Name : " + name + " is Reachable");			
+			}else{
+
+				System.out.println("is not Reachable : " + host); //testligne
+			}
+		}
+		System.out.println("Scan finished");
 	}
 }
